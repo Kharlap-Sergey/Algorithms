e653d25c6896a2a5795bd91faebdcbac2ecd2f03
siarhei kharlap <sergeygoodgood@gmailc.om>
2023-01-02 14:25:04 +0100

    Add stack

---
+++ Algorithms.Structures.Abstract/IStack.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithms.Structures.Abstract
+{
+    public interface IStack<T>
+    {
+        T Pop();
+        void Push(T item);
+
+        bool IsEmpty();
+    }
+}
---
+++ Algorithms.Structures.Concreet/Stack/LinkedListStack.cs
@@ -0,0 +1,33 @@
+﻿using Algorithms.Structures.Abstract;
+
+namespace Algorithms.Structures.Concreet.Stack
+{
+    public class LinkedListStack<T> : IStack<T>
+    {
+        private LinkedEntity<T>? _first = null;
+        public bool IsEmpty()
+        {
+            return _first == null;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty()) throw new Exception();
+
+            var temp = _first!;
+            _first = temp.Next;
+            return temp.Item;
+        }
+
+        public void Push(T item)
+        {
+            _first = new LinkedEntity<T> { Item = item, Next = _first };
+        }
+
+        private class LinkedEntity<T>
+        {
+            public T Item { get; set; }
+            public LinkedEntity<T>? Next { get; set; } = null;
+        }
+    }
+}
